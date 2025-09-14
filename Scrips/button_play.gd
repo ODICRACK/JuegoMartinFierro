8f34500d2038f86335af458864be01efcfadef08
ODICRACK <nahuel.odi.tv@gmail.com>
2025-09-13 21:38:17 -0300

    cambio de ecena al apretar play

--- Scrips/button_play.gd
+++ Scrips/button_play.gd
@@ -2,4 +2,5 @@ extends Button
 @onready var sonido2 = $"../chao"
 
 func _on_pressed() -> void:
+	load("res://Ecenas/Stardew.tscn") as PackedScene
 	sonido2.play()
--- Scrips/fundidor.gd
+++ Scrips/fundidor.gd
@@ -4,13 +4,14 @@ extends CanvasLayer
 func _on_fade_finished():
 	print("se terminó el fundido")
 	# Acá podés poner la acción que quieras (cambio de escena, etc.)
-
+	get_tree().change_scene_to_file("res://Ecenas/Stardew.tscn")
+	
 func _on_buttonquit_pressed() -> void:
 	var tween = get_tree().create_tween()
 	tween.tween_property(fade_rect, "modulate:a", 1.0, 1.0) # 1 seg de duración
 	# Usamos await para esperar a que termine
 	await tween.finished
-	_on_fade_finished()
+	
 
 
 func _on_button_play_pressed() -> void:
